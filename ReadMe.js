e7e2272f96081b67399f5e08395b93e84bc0cb0d
Tim Buzmakov <buzmakov.tim@gmail.com>
2022-04-23 22:53:19 -0700

    Delete lines

--- ReadMe.js
+++ ReadMe.js
@@ -1,7 +1,3 @@
 // Created from main computer in main branch
 // Change this from secondary computer
-// Another add ons from main computer
-// This from main computer!
-// Add ON from secondary computer
-// CHANGED!!! 
-// ANOTHER CHANGE!!!
+
